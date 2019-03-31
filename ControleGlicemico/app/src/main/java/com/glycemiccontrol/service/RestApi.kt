0f36aefa6e1b502ae9e3b6697d5f6c5fb5e67ded
lrmvasconcelos <lrmvasconcelos.dev@gmail.com>
2019-03-31 18:09:52 -0300

    Implementando cadamada de serviço

---
+++ ControleGlicemico/app/src/main/java/com/glycemiccontrol/service/RestApi.kt
@@ -0,0 +1,4 @@
+package com.glycemiccontrol.service
+
+interface RestApi {
+}
---
+++ ControleGlicemico/app/src/main/java/com/glycemiccontrol/service/RetrofitBase.kt
@@ -0,0 +1,59 @@
+package com.glycemiccontrol.service
+
+import com.glycemiccontrol.BuildConfig
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.reflect.Modifier
+import java.util.concurrent.TimeUnit
+
+class RetrofitBase {
+
+    private lateinit var interfaceRetrofit: RestApi
+    private lateinit var retrofit: Retrofit
+
+    init {
+        initRetrofit()
+    }
+
+    fun getInterfaceRetrofit(): RestApi? {
+        return interfaceRetrofit
+    }
+
+    private fun initRetrofit() {
+        val builder = OkHttpClient.Builder()
+        builder.readTimeout(1, TimeUnit.HOURS)
+        builder.connectTimeout(60, TimeUnit.SECONDS)
+
+        if (BuildConfig.DEBUG) {
+            val logInterceptor = HttpLoggingInterceptor()
+            logInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            builder.addInterceptor(logInterceptor)
+        }
+
+        val gsonBuilder = GsonBuilder()
+            .excludeFieldsWithModifiers(Modifier.FINAL, Modifier.TRANSIENT, Modifier.STATIC)
+
+        //        gsonBuilder.registerTypeAdapter(TypePayment.class, new TypePaymentDeserializer());
+        val gson = gsonBuilder.create()
+        val gsonConverterFactory = GsonConverterFactory.create(gson)
+
+        //        addAuthorizationHeaders(builder);
+
+        val urlBase = ""
+
+        val httpClient = builder.build()
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(urlBase)
+            .client(httpClient)
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+
+        interfaceRetrofit = retrofit.create(RestApi::class.java)
+    }
+}
