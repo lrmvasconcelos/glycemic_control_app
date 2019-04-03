cf16d22baa81a2dbea42993984bf95b6a4e09881
Madeira <lucasmadeira@ioasys.co>
2019-04-03 20:03:39 -0300

    Adiciona tela de seleção de tipo de usuário

---
+++ ControleGlicemico/app/src/main/java/com/glycemiccontrol/ui/PacientActivity.kt
@@ -0,0 +1,13 @@
+package com.glycemiccontrol.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.glycemiccontrol.R
+
+class PacientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pacient)
+    }
+}
