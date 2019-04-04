ab6215feb3506708f2b79c6d03b4d72ac5cbf562
Madeira <lucasmadeira@ioasys.co>
2019-04-04 11:09:06 -0300

    Criando adapter

---
+++ ControleGlicemico/app/src/main/java/com/glycemiccontrol/custom/GlycemicTestAdapter.kt
@@ -0,0 +1,33 @@
+package com.glycemiccontrol.custom
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.glycemiccontrol.R
+import kotlinx.android.synthetic.main.test_view.view.*
+import java.util.ArrayList
+
+class GlycemicTestAdapter(
+    private val items: MutableList<GlycemicModel> = ArrayList(),
+    private val callback: GlycemicTestView.OnItemClick
+    ) : RecyclerView.Adapter<GlycemicTestAdapter.ItemViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.test_view, parent, false);
+        return ItemViewHolder(view); }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        var item = items[position];
+        with(holder.itemView) {
+            glycemic_test_view.make(position, item, callback)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ItemViewHolder(view: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(view)
+}
